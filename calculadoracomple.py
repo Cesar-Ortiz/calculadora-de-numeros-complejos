f7f3eb193d9c654511c76a6dd78504798a15d78e
Cesar-Ortiz <54223085+Cesar-Ortiz@users.noreply.github.com>
2019-08-17 21:52:20 -0500

    Update calculadoracomple.py

--- calculadoracomple.py
+++ calculadoracomple.py
@@ -1,20 +1,28 @@
+#Operaciones de los numeros complejos
 import sys
 import math
 
 #En estes archivo estan contenidas las operaciones basicas de los numeros complejos
 
+#Suma
 def suma(a,b):
     c=a[0]+b[0]
     d=a[1]+b[1]
     return impri(c,d)
+
+#Resta
 def res(a,b):
     c=a[0]-b[0]
     d=a[1]-b[1]
     return impri(c,d)
+
+#Multiplicación
 def multi(a,b):
     c=a[0]*b[0]-a[1]*b[1]
     d=a[0]*b[1]+a[1]*b[0]
     return impri(c,d)
+
+#División
 def div(a,b):
     c=conju(b)
     e=multi(a,c)
@@ -23,6 +31,8 @@ def div(a,b):
     g=e[0]/x
     h=e[1]/x
     return impri(g,h)
+
+#Conjugado
 def conju(a):
     b=a[1]*-1
     return impri(a[0],b)
@@ -33,13 +43,18 @@ def impri(a,b):
     z.append(b)
     return z
 
+#Modulo
 def modu(a):
     b=(a[0]**2+a[1]**2)**(1/2)
     return b
+
+#Pasar de polar a cartesiano
 def pol_carte(a):
     b=a[0]*math.cos(a[1])
     c=a[0]*math.sin(a[1])
     return impri(b,c)
+
+#Pasar de cartesiano a polar
 def carte_pol(a):
     b=(a[0]**2+a[1]**2)**(1/2)
     c=math.atan(a[1]/a[0])
